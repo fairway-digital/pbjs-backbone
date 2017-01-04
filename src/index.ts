14b14be710596769932d638805475df63805d928
Adrien Frediani <frediani.adrien@gmail.com>
2017-01-04 22:45:37 +0100

    index: remove console.log

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,10 @@ namespace PBB {
     let packageName = Object.keys(root.nested)[0];
     store[packageName] = {};
 
-    console.log("package name: ", packageName);
-
     let messages = root.nested[packageName].nested;
-    console.log("messages: ", messages);
 
     Object.keys(messages).forEach((msg) => {
       let message = messages[msg];
-
-      console.log("message: ", message);
-      console.log("message defaults: ", PBB.builder.defaults(message));
-      console.log("message obj", messages[message]);
 
       class Class extends Backbone.Model {
         fullName = packageName + "." + message;

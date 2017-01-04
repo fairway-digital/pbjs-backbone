ed02d0cf510433ea8676a88fb8aadd45bdb345aa
Adrien Frediani <frediani.adrien@gmail.com>
2017-01-04 21:34:55 +0100

    add more tests

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,14 +1,32 @@
 protobuf.load("spec/proto/defaults.proto.json", function(err, root) {
+  PBB.load(root);
 
-  describe("PBB", function() {
-    it("must be defined", function() {
-      expect(PBB).toBeDefined();
-    });
-  });
+  describe("#PBB", function() {
+    describe(".getModel", function() {
+      it("must be defined", function() {
+        expect(PBB.getModel).toBeDefined();
+      });
 
-  describe("hello", function() {
-    it("must be hello", function() {
-      expect("hello").toBe("hello");
+      describe("must return a Backbone Model", function() {
+        var TestMessage, testMessage;
+
+        beforeEach(function() {
+          TestMessage = PBB.getModel("defaults", "TestMessage");
+          testMessage = new TestMessage();
+        });
+
+        it("Class", function() {
+          expect(TestMessage).toBeDefined();
+        });
+
+        it("instantiable", function() {
+          expect(testMessage).toBeDefined();
+        });
+
+        it("whith defaults correctly initialized", function() {
+          expect(testMessage.get("testField")).toBe("");
+        });
+      });
     });
   });
 

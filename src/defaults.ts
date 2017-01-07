733f653204154aaf880b18cb920de5d6ef5c059c
Adrien Frediani <frediani.adrien@gmail.com>
2017-01-07 13:42:40 +0100

    refacto: defaults

--- src/defaults.ts
+++ src/defaults.ts
@@ -2,27 +2,38 @@
 
 namespace PBB.builder {
 
+  type ScalarType = String | Boolean | Number;
+
+  function buildScalarDefault(field: any): ScalarType |  undefined {
+    switch (field.type) {
+      case "string":
+        return "";
+      case "bool":
+        return false;
+      case "double":
+      case "float":
+      case "int32":
+        return 0;
+      default:
+        return;
+    }
+  }
+
+  function buildDefault(field: any): any {
+    if (PBB.utils.isScalarType(field)) {
+      return buildScalarDefault(field);
+    } else {
+      return field.id;
+    }
+  }
+
   export function defaults(message: any): any {
     let defaults = {};
 
     Object.keys(message.fields).forEach((fieldName) => {
       const field = message.fields[fieldName];
 
-      if (PBB.utils.isScalarType(field)) {
-        if (field.type === "string") {
-          defaults[field.name] = "";
-        }
-        else if (field.type === "bool") {
-          defaults[field.name] = false;
-        }
-        else if (field.type === "double"
-          || field.type === "float" || field.type === "int32") {
-            defaults[field.name] = 0;
-          }
-      } else {
-        defaults[field.name] = field.id;
-      }
-
+      defaults[field.name] = buildDefault(field);
     });
 
     return defaults;

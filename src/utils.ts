0272ca5b746c6b02ad9f3df957597e33290181ff
Adrien Frediani <frediani.adrien@gmail.com>
2017-01-11 10:29:33 +0100

    utils: introduce store interface and memory impl

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,12 @@
 
 namespace PBB.utils {
 
+  interface IStore {
+    get(key: string): any;
+    push(key: string, obj: any): void;
+    has(key: string): boolean;
+  }
+
   export function isEnum(protoObj: any): boolean {
     return (typeof protoObj.values !== "undefined")
     && (typeof protoObj.valuesById !== "undefined");
@@ -9,6 +15,32 @@ namespace PBB.utils {
 
   export function isScalarType(protoObj: any): boolean {
     return protobuf.types.basic.hasOwnProperty(protoObj.type);
+  }
+
+  export class MemoryStore implements IStore {
+    private store: any;
+
+    constructor() {
+      this.store = {};
+    }
+
+    get(key: string): any {
+      if (this.has(key)) {
+        return this.store[key];
+      }
+    }
+
+    has(key: string): boolean {
+      return this.store.hasOwnProperty(key);
+    }
+
+    push(key: string, obj: any, force: boolean = false) {
+      if (!this.has(key)) {
+        this.store[key] = obj;
+      } else if (force) {
+        this.store[key] = obj;
+      }
+    }
   }
 
 }

31f3f1bf5298d8949faa234b390f6bb4a54906aa
Adrien Frediani <frediani.adrien@gmail.com>
2017-01-11 10:37:33 +0100

    enums: use utils store

--- src/enum.ts
+++ src/enum.ts
@@ -1,18 +1,18 @@
 /// <reference path="../node_modules/@types/backbone/index.d.ts" />
+/// <reference path="utils.ts" />
 
 namespace PBB.enumeration {
-  let enumStore = {};
+
+  const store = new MemoryStore();
 
   export function get(pkg: string, name: string): any {
-    return enumStore[`.${pkg}.${name}`];
+    const fullName = `.${pkg}.${name}`;
+
+    return store.get(fullName);
   }
 
   export function push(obj: any) {
-    if (!enumStore[obj.fullName]) {
-      enumStore[obj.fullName] = {};
-    }
-
-    enumStore[obj.fullName] = obj.values;
+    store.push(obj.fullName, obj.values);
   }
 }
 

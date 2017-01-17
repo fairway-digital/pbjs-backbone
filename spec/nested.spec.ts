1da283085ee987fe4447e1c304d00e28f57abb55
Adrien Frediani <frediani.adrien@gmail.com>
2017-01-17 22:57:45 +0100

    specs: add test sfor imported proto message (but failing)

--- spec/nested.spec.ts
+++ spec/nested.spec.ts
@@ -4,21 +4,30 @@
 describe("#shaft.model", () => {
     describe("Nested models", () => {
       let MessageContainer, messageContainer;
+      let MessageContainer2, messageContainer2;
 
       beforeEach(() => {
         MessageContainer = shaft.model.get("nested", "MessageContainer");
         messageContainer = new MessageContainer();
+        MessageContainer2 = shaft.model.get("nested", "MessageContainer2");
+        messageContainer2 = new MessageContainer2();
       });
 
       afterEach(() => {
         MessageContainer = null;
         messageContainer = null;
+        MessageContainer2 = null;
+        messageContainer2 = null;
       });
 
       it("should be attached", () => {
         expect(messageContainer.get("inner")).toBeDefined();
         expect(messageContainer.get("inner").get("innerMessage2")).toBeDefined();
         expect(messageContainer.get("inner").get("innerMessage2").get("name")).toBeDefined();
+      });
+
+      it("should be attached as imported proto message", () => {
+        expect(messageContainer2.get("importedMessage").get("name")).toBe("");
       });
 
     });

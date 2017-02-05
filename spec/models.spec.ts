06c939e2bad14fae746f2170119a298752e44bd6
Adrien Frediani <frediani.adrien@gmail.com>
2017-02-05 15:57:50 +0100

    dts: rename shaft-> index

--- spec/models.spec.ts
+++ spec/models.spec.ts
@@ -1,5 +1,5 @@
 /// <reference path="../node_modules/@types/jasmine/index.d.ts" />
-/// <reference path="../shaft.d.ts" />
+/// <reference path="../index.d.ts" />
 
 describe("#shaft.model", () => {
   describe(".get", () => {

b62ceb6ca0fec3b4333cb381679d45a7f5ad476e
Adrien Frediani <frediani.adrien@gmail.com>
2017-02-05 15:05:24 +0100

    shaft: introduce d.ts

---
+++ shaft.d.ts
@@ -0,0 +1,13 @@
+export as namespace shaft;
+
+export namespace model {
+  export function get(pkg: string, name: string): any;
+}
+
+export namespace collection {
+  export function get(pkg: string, name: string): any;
+}
+
+export namespace enumeration {
+  export function get(pkg: string, name: string): any;
+}
